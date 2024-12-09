4a329c3c7c212c3f5dcc0726cca3d05f6d32ddb8
nicoladima <nicoladimartino@gmail.com>
2024-12-09 14:53:19 +0100

    gne

--- src/components/AppSidebar.js
+++ src/components/AppSidebar.js
@@ -37,8 +37,9 @@ const AppSidebar = () => {
     >
       <CSidebarHeader className="border-bottom">
         <CSidebarBrand to="/">
-          <CIcon customClassName="sidebar-brand-full" icon={logo} height={32} />
-          <CIcon customClassName="sidebar-brand-narrow" icon={sygnet} height={32} />
+        <h5>WorkFlowDima's</h5>
+          {/*<CIcon customClassName="sidebar-brand-full" icon={logo} height={32} />*/}
+          {/*<CIcon customClassName="sidebar-brand-narrow" icon={sygnet} height={32} />*/}
         </CSidebarBrand>
         <CCloseButton
           className="d-lg-none"
