4d80c9759a51ab90d7f1f606a205adea8d719187
nicoladima63 <nicoladimartino@gmail.com>
2025-03-10 21:35:53 +0100

    cancellato i doppioni

--- zzdoppioni/xSetup.js
+++
@@ -1,9 +0,0 @@
-import SetupWizard from './SetupWizard';
-import WizardContainer from './WizardContainer';
-// Nel tuo componente padre o pagina
-function SetupPage() {
-  //return <SetupWizard />;
-  return <WizardContainer />;
-}
-
-export default SetupPage;
